42fa8ec9ff0b3c64d71c0fe1cc392f045ecb3fde
Ikko Fushiki <ifushiki@yahoo.com>
2014-12-23 21:30:19 -0800

    sendImageData is rename to receiveImageData.

--- LiveVideo/LiveVideoCaptureManager.m
+++ LiveVideo/LiveVideoCaptureManager.m
@@ -330,12 +330,12 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
         if(USE_FILTER_LAYER)
         {
             if (self.videoOutputLayer2 && [self.videoOutputLayer2.imagePipe isReadyToReceiveNewData] == YES)
-                [(DwVideoOutputLayer *) self.videoOutputLayer2 sendImageData:dataBuffer withBytesPerRow:bytesPerRow withWidth:width withHeight:height];
+                [(DwVideoOutputLayer *) self.videoOutputLayer2 receiveImageData:dataBuffer withBytesPerRow:bytesPerRow withWidth:width withHeight:height];
 
         }
         else  {
             if (self.videoOutputView2 && [self.videoOutputView2.imagePipe isReadyToReceiveNewData] == YES)
-                [(DwVideoOutputView *) self.videoOutputView2 sendImageData:dataBuffer withBytesPerRow:bytesPerRow withWidth:width withHeight:height];
+                [(DwVideoOutputView *) self.videoOutputView2 receiveImageData:dataBuffer withBytesPerRow:bytesPerRow withWidth:width withHeight:height];
         }
         
         
